73c8a86f5b5b9db29d021be0e445d5481b96d5c3
lfmundim <lucasfmundim@ufmg.br>
2018-09-17 22:46:20 -0300

    Started swapping NUnit for XUnit

--- DataValidator.Tests/DateUnitTests.cs
+++ DataValidator.Tests/DateUnitTests.cs
@@ -1,41 +1,41 @@
-﻿using DataValidator.Date;
-using NUnit.Framework;
-using Shouldly;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using DataValidator.Date;
+//using NUnit.Framework;
+//using Shouldly;
+//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace DataValidator.Tests
-{
-    [TestFixture]
-    public class DateUnitTests
-    {
-        public ICheckDate checkDate { get; set; }
+//namespace DataValidator.Tests
+//{
+//    [TestFixture]
+//    public class DateUnitTests
+//    {
+//        public ICheckDate checkDate { get; set; }
 
-        [OneTimeSetUp]
-        public void Config()
-        {
-            checkDate = new CheckDate();
-        }
+//        [OneTimeSetUp]
+//        public void Config()
+//        {
+//            checkDate = new CheckDate();
+//        }
 
-        [Test]
-        [TestCase("O natal esse ano acontece no dia 25/12/2018", "MM/dd/yyyy", true, false, "12/25/2018", true)]
-        [TestCase("The D-Day happened on 6/6/1944", "dd/MM/yyyy", false, false, "06/06/1944", true)]
-        [TestCase("Albert Einstein was born on 14/03/1879", "dd/MM/yyyy", false, false, "14/03/1879", true)]
-        [TestCase("Só vou fazer isso dia 31/02/2019", "dd/MM/yyyy", false, false, "01/01/0001", false)]
-        [TestCase("Não tem data aqui e agora", "dd/MM/yyyy", false, false, "", false)]
-        public void TestCheckDate(string text, string format, bool isHoliday, bool isWeekend, string expectedOutput, bool isValid)
-        {
-            // Arrange
+//        [Test]
+//        [TestCase("O natal esse ano acontece no dia 25/12/2018", "MM/dd/yyyy", true, false, "12/25/2018", true)]
+//        [TestCase("The D-Day happened on 6/6/1944", "dd/MM/yyyy", false, false, "06/06/1944", true)]
+//        [TestCase("Albert Einstein was born on 14/03/1879", "dd/MM/yyyy", false, false, "14/03/1879", true)]
+//        [TestCase("Só vou fazer isso dia 31/02/2019", "dd/MM/yyyy", false, false, "01/01/0001", false)]
+//        [TestCase("Não tem data aqui e agora", "dd/MM/yyyy", false, false, "", false)]
+//        public void TestCheckDate(string text, string format, bool isHoliday, bool isWeekend, string expectedOutput, bool isValid)
+//        {
+//            // Arrange
 
-            // Act
-            var result = checkDate.ValidateAndFormatDate(text, format);
+//            // Act
+//            var result = checkDate.ValidateAndFormatDate(text, format);
 
-            // Arrange
-            result.IsHoliday.ShouldBe(isHoliday);
-            result.IsWeekend.ShouldBe(isWeekend);
-            result.Value.ShouldBe(expectedOutput);
-            result.IsValid.ShouldBe(isValid);
-        }
-    }
-}
+//            // Arrange
+//            result.IsHoliday.ShouldBe(isHoliday);
+//            result.IsWeekend.ShouldBe(isWeekend);
+//            result.Value.ShouldBe(expectedOutput);
+//            result.IsValid.ShouldBe(isValid);
+//        }
+//    }
+//}
